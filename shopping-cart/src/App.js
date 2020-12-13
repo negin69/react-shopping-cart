089491e7a924f6f227a5886e694c47ff0738baab
unknown <negin_mtn@yahoo.com>
2020-12-13 16:38:30 +0330

    First commit

---
+++ shopping-cart/src/App.js
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div className="App">
+      <header>
+       <a href="/">React Shopping Cart</a>
+      </header>
+      <main>
+       Product List
+      </main>
+
+      <footer>
+        <p>all things are reserved</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
