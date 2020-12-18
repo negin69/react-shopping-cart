ae0a75184a36ace90156f745946525621b804916
negin69 <negin_mtn@yahoo.com>
2020-12-19 01:13:10 +0330

    change store file

--- shopping-cart/src/store.js
+++ shopping-cart/src/store.js
@@ -1,16 +1,36 @@
-import { createStore, applyMiddleware, compose, combineReducers } from "redux";
+//First solution
+
+// import { createStore, applyMiddleware, compose, combineReducers } from "redux";
+// import thunk from "redux-thunk";
+// import { productReducers } from "./reducers/productReducers";
+
+// const initialState = {};
+// const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// const store = createStore(
+//   combineReducers({
+//     products: productReducers,
+   
+//   }),
+//   initialState,
+//   composeEnhancer(applyMiddleware(thunk))
+// );
+// export default store;
+
+
+//Second solution
+import { createStore, applyMiddleware, compose , combineReducers} from "redux";
 import thunk from "redux-thunk";
 import { productReducers } from "./reducers/productReducers";
-
-
 const initialState = {};
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(
+export const store = createStore(
   combineReducers({
-    products: productReducers,
-   
-  }),
-  initialState,
-  composeEnhancer(applyMiddleware(thunk))
+         products: productReducers,     
+     }),
+   initialState,
+    compose(
+        applyMiddleware(thunk),
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+            window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
 );
 export default store;
